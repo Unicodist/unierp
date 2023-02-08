7d4cd80c937e92e74e3eeb07346727bbcf835df1
Roshan Limbu <roshanlimbu2075@gmail.com>
2023-02-08 17:15:29 +0545

    Created login page

---
+++ pages/purchase/new.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class Purchase extends Component{
+    render(){
+        return(
+            <div>
+                Purchase
+            </div>
+        )
+    }
+}
+export default Purchase;
---
+++ pages/sales/new.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class  Sales extends Component{
+    render(){
+        return(
+            <div>
+                Sales
+            </div>
+        )
+    }
+}
+export default Sales;
